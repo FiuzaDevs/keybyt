8a467975fd8a87d0f63d5e46947fc517b02e9e0b
Pedro Fiuza <62244984+FiuzaDevs@users.noreply.github.com>
2021-10-01 00:04:42 -0300

    Delete constants.ts

--- src/services/constants.ts
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = "https://vkaeuedswexyubwdmnwc.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDE1NDQwNywiZXhwIjoxOTM1NzMwNDA3fQ.BrFriW7vKHEpU7jLQAS3QrNhcnOtVa87HiB7AzdOBTI";
-
