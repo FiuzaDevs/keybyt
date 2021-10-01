54d816919e31b3537b3d30d8c2c16e832d14890e
Pedro Fiuza <62244984+FiuzaDevs@users.noreply.github.com>
2021-10-01 00:05:09 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const SUPABASE_URL = "https://vkaeuedswexyubwdmnwc.supabase.co";
+const SUPABASE_URL = "any";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDE1NDQwNywiZXhwIjoxOTM1NzMwNDA3fQ.BrFriW7vKHEpU7jLQAS3QrNhcnOtVa87HiB7AzdOBTI";
+  "any";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   localStorage: AsyncStorage as any,
